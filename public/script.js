82e9e8eff6323246b17f3454e8f6e1ca1a17b58a
João Vitor Ferreira <84388644+Joaovitor1998@users.noreply.github.com>
2024-11-27 14:12:14 -0300

    Update script.js
    - Clean form values when submitted

--- public/script.js
+++ public/script.js
@@ -3,17 +3,28 @@ const form = document.getElementById('user-form');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
+
+    // Pra poder limpar o formulário depois de submetido
+    const nameElement = document.getElementById('name');
+    const emailElement = document.getElementById('email');
 
     fetch('/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ name, email }),
+        body: JSON.stringify({
+            name: nameElement.value,
+            email: emailElement.value
+        }),
     })
         .then(response => response.text())
-        .then(data => alert(data))
+        .then(data => {
+            alert(data)
+
+            // Aqui limpa o formulário depois que foi submetido
+            nameElement.value = ""
+            emailElement.value = ""
+        })
         .catch(error => console.error('Erro:', error));
 });
